8afa5e7e031a7d5d507a7bf15f3b3f0dc3be8915
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2025-08-25 17:36:50 +0000

    art gallery initiated

---
+++ __3-art-gallery/src/main.js
@@ -0,0 +1,1 @@
+console.log("Art gallery initiated");
---
+++ __3-art-gallery/vite.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('vite').UserConfig}
+ * @see https://vitejs.dev/config/
+ * @see https://vitejs.dev/guide/build.html#build-options
+ * @see https://vitejs.dev/guide/assets.html#the-public-directory
+ * @see https://vitejs.dev/guide/static-deploy.html#basic-usage
+ * @see https://vitejs.dev/guide/static-deploy.html#github-pages
+ * @see https://vitejs.dev/guide/static-deploy.html#netlify
+ * @see https://vitejs.dev/guide/static-deploy.html#vercel
+ */
+
+export default {
+  publicDir: "public",
+  build: {
+    outDir: "build",
+  },
+};
