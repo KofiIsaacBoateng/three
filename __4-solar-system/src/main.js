ff2135017571d94f7c8db4bac96fdbb8ad825519
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2025-08-28 11:03:12 +0000

    galaxy and sun added

--- __4-solar-system/src/main.js
+++ __4-solar-system/src/main.js
@@ -1,46 +1,60 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
+import planets, { sunMaterial, cubeMap } from "./planets";
 
 // get dom elements
 const canvas = document.querySelector(".canvas");
 
+const galaxyCube = new THREE.CubeTexture();
 // create a scene
 const scene = new THREE.Scene();
+scene.background = cubeMap;
 
 // add objects
 const sphere = new THREE.SphereGeometry(2, 32, 16);
-const material = new THREE.MeshBasicMaterial({
-  color: "aqua",
-  wireframe: true,
-});
-const mesh = new THREE.Mesh(sphere, material);
+
+// create mesh
+const mesh = new THREE.Mesh(sphere, sunMaterial);
 scene.add(mesh);
+
+// add light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 1000, 1000);
+pointLight.position.set(0, 5, 15);
+scene.add(pointLight);
 
 // create a camera
 const camera = new THREE.PerspectiveCamera(
-  120,
+  35,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  400
 );
-camera.position.z = 5;
+camera.position.set(0, 5, 15);
 
 // create renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.toneMapping = THREE.NeutralToneMapping;
+renderer.toneMappingExposure = 1;
 renderer.setAnimationLoop(renderLoop);
 
 // orbitals
 const orbitals = new OrbitControls(camera, renderer.domElement);
 orbitals.autoRotate = true;
 orbitals.enableDamping = true;
-orbitals.autoRotateSpeed = 10;
+orbitals.maxDistance = 200;
+orbitals.minDistance = 5;
 
 // add renderer to dom (must be done after orbit controls)
 document.body.appendChild(renderer.domElement);
 
 function renderLoop() {
+  mesh.rotation.y += 0.01;
+
   orbitals.update();
   renderer.render(scene, camera);
 }
---
+++ __4-solar-system/src/planets.js
@@ -0,0 +1,91 @@
+import * as THREE from "three";
+
+// texture loader
+const textureLoader = new THREE.TextureLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+cubeTextureLoader.setPath("templates/cubeMap/");
+
+export const mercuryMaterial = new THREE.MeshStandardMaterial({
+  map: textureLoader.load("templates/mercury.jpg"),
+});
+export const venusMaterial = new THREE.MeshStandardMaterial({
+  map: textureLoader.load("templates/venus.jpg"),
+});
+export const earthMaterial = new THREE.MeshStandardMaterial({
+  map: textureLoader.load("templates/earth.jpg"),
+});
+export const marsMaterial = new THREE.MeshStandardMaterial({
+  map: textureLoader.load("templates/mars.jpg"),
+});
+export const moonMaterial = new THREE.MeshStandardMaterial({
+  map: textureLoader.load("templates/moon.jpg"),
+});
+
+export const sunMaterial = new THREE.MeshStandardMaterial({
+  map: textureLoader.load("templates/sun.jpg"),
+});
+
+export const cubeMap = cubeTextureLoader.load([
+  "px.png",
+  "nx.png",
+  "py.png",
+  "ny.png",
+  "nz.png",
+  "pz.png",
+]);
+
+export default [
+  {
+    name: "Mercury",
+    radius: 0.5,
+    distance: 10,
+    speed: 0.01,
+    material: mercuryMaterial,
+    moons: [],
+  },
+  {
+    name: "Venus",
+    radius: 0.8,
+    distance: 15,
+    speed: 0.007,
+    material: venusMaterial,
+    moons: [],
+  },
+  {
+    name: "Earth",
+    radius: 1,
+    distance: 20,
+    speed: 0.005,
+    material: earthMaterial,
+    moons: [
+      {
+        name: "Moon",
+        radius: 0.3,
+        distance: 3,
+        speed: 0.015,
+      },
+    ],
+  },
+  {
+    name: "Mars",
+    radius: 0.7,
+    distance: 25,
+    speed: 0.003,
+    material: marsMaterial,
+    moons: [
+      {
+        name: "Phobos",
+        radius: 0.1,
+        distance: 2,
+        speed: 0.02,
+      },
+      {
+        name: "Deimos",
+        radius: 0.2,
+        distance: 3,
+        speed: 0.015,
+        color: 0xffffff,
+      },
+    ],
+  },
+];
