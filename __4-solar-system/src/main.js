d39351e008b99d1f3975e6620660a4c6c0c9f94a
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2025-08-28 09:52:34 +0000

    three js basic setup

--- __4-solar-system/src/main.js
+++ __4-solar-system/src/main.js
@@ -1,24 +1,46 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// get dom elements
+const canvas = document.querySelector(".canvas");
 
-setupCounter(document.querySelector('#counter'))
+// create a scene
+const scene = new THREE.Scene();
+
+// add objects
+const sphere = new THREE.SphereGeometry(2, 32, 16);
+const material = new THREE.MeshBasicMaterial({
+  color: "aqua",
+  wireframe: true,
+});
+const mesh = new THREE.Mesh(sphere, material);
+scene.add(mesh);
+
+// create a camera
+const camera = new THREE.PerspectiveCamera(
+  120,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+// create renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setAnimationLoop(renderLoop);
+
+// orbitals
+const orbitals = new OrbitControls(camera, renderer.domElement);
+orbitals.autoRotate = true;
+orbitals.enableDamping = true;
+orbitals.autoRotateSpeed = 10;
+
+// add renderer to dom (must be done after orbit controls)
+document.body.appendChild(renderer.domElement);
+
+function renderLoop() {
+  orbitals.update();
+  renderer.render(scene, camera);
+}
