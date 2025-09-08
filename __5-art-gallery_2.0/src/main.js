a0f72ce9949af3e1299e48c7e213241d1f466900
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2025-09-08 07:07:14 +0000

    gallery walls, ceiling and floor

--- __5-art-gallery_2.0/src/main.js
+++ __5-art-gallery_2.0/src/main.js
@@ -1,1 +1,53 @@
-console.log("Art gallery 2.0 initiated");
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { PointerLockControls } from "three/examples/jsm/Addons.js";
+import addWallsToScene from "./walls";
+
+// initialize scene
+const scene = new THREE.Scene();
+
+// initialize camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 45;
+
+// initialize renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.toneMapping = THREE.NeutralToneMapping;
+renderer.toneMappingExposure = 1.5;
+// renderer.outputEncoding = THREE.sRGBEncoding
+renderer.setAnimationLoop(renderLoop);
+document.body.appendChild(renderer.domElement);
+
+// initialize orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.position0.set(0, 0, 0);
+controls.enableDamping = true;
+controls.minDistance = 0;
+controls.maxDistance = 50;
+controls.zoomSpeed = 3;
+
+// initialize pointer lock controls
+// const pointerControls = new PointerLockControls(camera, renderer.domElement);
+
+function renderLoop() {
+  controls.update();
+  //   pointerControls.update();
+  renderer.render(scene, camera);
+}
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// add a objects here
+const walls = addWallsToScene(scene);
---
+++ __5-art-gallery_2.0/src/walls.js
@@ -0,0 +1,80 @@
+import * as THREE from "three";
+
+const walls = [
+  {
+    name: "left-wall",
+    width: 100,
+    height: 50,
+    color: 0x071739,
+    position: new THREE.Vector3(-50, 0, 0),
+    rotation: new THREE.Vector3(0, Math.PI / 2, 0),
+  },
+  {
+    name: "right-wall",
+    width: 100,
+    height: 50,
+    color: 0x071739,
+    position: new THREE.Vector3(50, 0, 0),
+    rotation: new THREE.Vector3(0, -Math.PI / 2, 0),
+  },
+  {
+    name: "back-wall",
+    width: 100,
+    height: 50,
+    color: 0x071739,
+    position: new THREE.Vector3(0, 0, -50),
+    rotation: new THREE.Vector3(0, 0, 0),
+  },
+  {
+    name: "front-wall",
+    width: 100,
+    height: 50,
+    color: 0x071739,
+    position: new THREE.Vector3(0, 0, 50),
+    rotation: new THREE.Vector3(0, Math.PI, 0),
+  },
+  {
+    name: "floor",
+    width: 100,
+    height: 100,
+    color: 0xe3c390,
+    position: new THREE.Vector3(0, -25, 0),
+    rotation: new THREE.Vector3(-Math.PI / 2, 0, 0),
+  },
+  {
+    name: "ceiling",
+    width: 100,
+    height: 100,
+    color: 0x4b6382,
+    position: new THREE.Vector3(0, 25, 0),
+    rotation: new THREE.Vector3(Math.PI / 2, 0, 0),
+  },
+];
+
+export function createWall(width, height, color, position, rotation) {
+  const geometry = new THREE.PlaneGeometry(width, height);
+  const material = new THREE.MeshBasicMaterial({
+    color: color,
+    side: THREE.DoubleSide,
+  });
+  const wall = new THREE.Mesh(geometry, material);
+  wall.position.set(position.x, position.y, position.z);
+  wall.rotation.set(rotation.x, rotation.y, rotation.z);
+  return wall;
+}
+
+export default function addWallsToScene(scene) {
+  walls.forEach((wallData) => {
+    const wall = createWall(
+      wallData.width,
+      wallData.height,
+      wallData.color,
+      wallData.position,
+      wallData.rotation
+    );
+    wall.name = wallData.name;
+    scene.add(wall);
+  });
+
+  return walls;
+}
